af4bc9be3f80825a614b7d9045b48f96765f9469
DiegoAs868 <73847019+DiegoAs868@users.noreply.github.com>
2021-01-18 10:41:24 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//TIPOS DE DTOS EN JAVASCRIPT
+var saludo  "hola JavaScript"
+var edad = 26
+var unPoco = 1.5
+var siNo = true
+var seccion - "a"
+
+//MOSTRAR UN DATO CONSOLA
+console.log(saludo)
+
+
+
+var datoApellido = document.getElementById('favorito')
+
